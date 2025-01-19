c794cbc0281a0a8cf61438e8df1768274e7c127a
DanielRubianes <DanielRubianes1352@gmail.com>
2025-01-19 07:08:24 -0500

    Add settings page template

---
+++ source/js/config.js
@@ -0,0 +1,55 @@
+(async (PLUGIN_ID) => {
+  'use strict';
+
+  // escape values
+  const escapeHtml = (htmlstr) => {
+    return htmlstr
+      .replace(/&/g, '&amp;')
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;')
+      .replace(/"/g, '&quot;')
+      .replace(/'/g, '&#39;')
+      .replace(/\n/g, '&#xA;');
+  };
+
+  // get form information
+  const searchFieldsFormData = document.getElementById('search-fields');
+
+  // get configuration settings
+  const config = kintone.plugin.app.getConfig(PLUGIN_ID);
+
+  // set initial value
+  searchFieldsFormData.value = config.searchFields || '';
+
+  // get app id
+  const appId = kintone.app.getId();
+
+  // save button
+  const saveButton = document.getElementById('submit');
+  saveButton.addEventListener('click', () => {
+    const searchFields = escapeHtml(searchFieldsFormData.value);
+    // required values check
+    if (searchFields === '') {
+      alert('Required value is missing.');
+      return;
+    }
+    // duplicated values check
+    if (new Set([searchFields]).size < 2) {
+      alert('Duplicate values.');
+      return;
+    }
+    // save plugin configuration settings
+    const newConfig = {searchFields};
+    kintone.plugin.app.setConfig(newConfig, () => {
+      // redirect to the app settings page
+      window.location.href = `/k/admin/app/flow?app=${appId}`;
+    });
+  });
+
+  // cancel button
+  const cancelButton = document.getElementById('cancel');
+  cancelButton.addEventListener('click', () => {
+    // redirect to the list of plug-ins screen after clicking the cancel button
+    window.location.href = `/k/admin/app/${appId}/plugin/`;
+  });
+})(kintone.$PLUGIN_ID);
--- source/js/desktop.js
+++ source/js/desktop.js
@@ -1,71 +1,16 @@
 (() => {
     'use strict';
-  
-    // field codes
-    const telFieldCode = 'telephone_number';
-    const mailFieldCode = 'email';
-  
-    // check the Telephone value
-    const validateTel = (event) => {
-      // set the telephone pattern, number must be 10 or 11 digits
-      const telPattern = /^\d{10,11}$/;
-      const record = event.record;
-      record[telFieldCode].error = null;
-  
-      // check the input value
-      const telFieldValue = record[telFieldCode].value;
-      if (telFieldValue) {
-        if (!(telFieldValue.match(telPattern))) {
-          // error if the input value does not match the pattern
-          record[telFieldCode].error = 'Telephone number must be 10 or 11 digits.';
-        }
-      }
-    };
-  
-    // check the Email value
-    const validateMail = (event) => {
-      // set the email pattern
-      const mailPattern = /^([a-zA-Z0-9])+([a-zA-Z0-9._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9._-]+)+$/;
-      const record = event.record;
-      record[mailFieldCode].error = null;
-      // check the input value
-      const mailFieldValue = record[mailFieldCode].value;
-      if (mailFieldValue) {
-        if (!(mailFieldValue.match(mailPattern))) {
-          // error if the input value does not match the pattern
-          record[mailFieldCode].error = 'Invalid email address.';
-        }
-      }
-    };
-  
-    // Telephone field change event
-    kintone.events.on([
-      'app.record.create.change.' + telFieldCode,
-      'app.record.edit.change.' + telFieldCode,
-      'app.record.index.edit.change.' + telFieldCode
-    ], (event) => {
-      validateTel(event);
-      return event;
-    });
-  
-    // Email field change event
-    kintone.events.on([
-      'app.record.create.change.' + mailFieldCode,
-      'app.record.edit.change.' + mailFieldCode,
-      'app.record.index.edit.change.' + mailFieldCode
-    ], (event) => {
-      validateMail(event);
-      return event;
-    });
-  
-    // record save events
-    kintone.events.on([
-      'app.record.create.submit',
-      'app.record.edit.submit',
-      'app.record.index.edit.submit'
-    ], (event) => {
-      validateTel(event);
-      validateMail(event);
-      return event;
-    });
-  })();
+
+    // get plugin config
+    const config = kintone.plugin.app.getConfig(PLUGIN_ID) || {};
+
+    // exit if no settings selected
+    if (Object.keys(config).length === 0) {
+      return;
+    }
+
+    // Search fields field code
+    const searchFields = config.searchFields
+
+    
+})();
